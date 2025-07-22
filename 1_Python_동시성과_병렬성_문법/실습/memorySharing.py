9e59e5c3234f5fc4a8af1cf624743014f487f2ef
MaiBoii <20183010@gs.cwnu.ac.kr>
2025-07-22 21:37:41 +0900

    feat: 병렬성과 멀티프로세싱

---
+++ 1_Python_동시성과_병렬성_문법/실습/memorySharing.py
@@ -0,0 +1,42 @@
+
+from multiprocessing import Process, current_process, Value, Array
+import random
+import os
+
+def generate_update_number(v : int):
+    for i in range(50):
+        v.value += 1
+    print(current_process().name, "data", v.value)
+
+def main():
+    # 부모 프로세스 아이디
+    parent_process_id = os.getpid()
+    # 출력
+    print(f"Parent process ID {parent_process_id}")
+
+    # 프로세스 리스트  선언
+    processes = list()
+
+    # 프로세스 메모리 공유 변수
+    # from multiprocessing import shared_memory 사용 가능(파이썬 3.8)
+    # from multiprocessing import Manager 사용 가능
+    
+    # share_numbers = Array('i', range(50)) # i, c 등 Flag 확인
+    share_value = Value('i', 0)
+    for _ in range(1,10):
+        # 생성
+        p = Process(target=generate_update_number, args=(share_value,))
+        # 배열에 담기
+        processes.append(p)
+        # 실행
+        p.start()
+        
+    # Join
+    for p in processes:
+        p.join()
+
+    # 최종 프로세스 부모 변수 확인
+    print("Final Data(share_value) in parent process",  share_value.value)
+
+if __name__ == '__main__':
+    main()
